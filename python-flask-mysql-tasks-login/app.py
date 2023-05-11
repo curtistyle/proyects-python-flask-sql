12f19dfa9f2837848665f6abed2332ed1103d807
Sebastian Maldonado <58012659+curtistyle@users.noreply.github.com>
2023-05-11 19:46:18 -0300

    Update

--- python-flask-mysql-tasks-login/app.py
+++ python-flask-mysql-tasks-login/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_mysqldb import MySQL
 from config import Config
 
@@ -9,17 +9,43 @@ db = MySQL(app)
 config = Config()
 
 @app.route('/', methods=['GET'])
-def Index():
+def index():
+    cur = db.connection.cursor()
+    cur.execute("SELECT * FROM users")
+    user = cur.fetchone()
+    print(user)
+    cur.close()
+    
     return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-def Login():
+def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
         
+        cur = db.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        user = cur.fetchone()
+        cur.close()
+        if user is not None:
+            session['email'] = email
+            session['name'] = user[1]
+            session['surname'] = user[2]
+            
+            return redirect(url_for('tasks'))
+        else:
+            return render_template('index.html', message='Las credenciales no son correctas')
+    else:
+        render_template('index.html')
+        
+@app.route('/tasks', methods=['GET'])
+def tasks():
+    return render_template('tasks.html')  
+ 
         
 if __name__=='__main__':
     app.config.from_object(config)
     app.run()
+    
     
--- python-flask-mysql-tasks-login/config.py
+++ python-flask-mysql-tasks-login/config.py
@@ -1,7 +1,9 @@
+# config db
 class Config():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'admin'
     MYSQL_DB = 'flask_login'
+    SECRET_KEY = 'secretkey'
         
