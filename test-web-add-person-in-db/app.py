e0bd4901d38840c5dccf65e612ec3cca5161538f
Sebastian Maldonado <58012659+curtistyle@users.noreply.github.com>
2023-08-21 18:59:33 -0300

    update

--- test-web-add-person-in-db/app.py
+++ test-web-add-person-in-db/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_mysqldb import MySQL
 from cfg import Config_db
 from jinja2 import *
+from methods import input_band
 
 app = Flask(__name__)
 
@@ -9,10 +10,19 @@ db = MySQL(app)
 
 @app.route('/')
 def Index():
+    input_band()
     return render_template('index.html')
 
+@app.route('/list')
+def List():
+    return render_template('list.html')
 
+@app.route('/search')
+def Search():
+    
+    return redirect('Index')
 
 if __name__=='__main__':
     app.config.from_object(Config_db)
     app.run()
+
---
+++ test-web-add-person-in-db/methods.py
@@ -0,0 +1,14 @@
+import requests
+from flask import request
+
+
+
+
+
+def input_band():
+    name = request.args['name']
+    print(name)
+    
+def output_band(band):
+    
+
